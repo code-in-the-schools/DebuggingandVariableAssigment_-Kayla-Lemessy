d0909ec93a8e0825b1f43f76a348e46c86350bb5
Kayla Lemessy <kaylalemessy15@gmail.com>
2020-07-15 16:33:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a="Elia Deppe"
+b ="Earl Sweatshirt "
+c ="Norsara"
+d="painting and skateboarding" 
+e="7/15/20"
+
+print("My name is Elia Deppe")
+print("I love to go on to vacation in Norsara and my two favorite hobbies are painting and skateboarding ")
+print(" Today date is:7/15/20 ")
+
+
+
+
